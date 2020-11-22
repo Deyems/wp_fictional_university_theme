b5b046d9687bbed34414b222da9dd94a36fd9eba
deyems <adeyemisola11@gmail.com>
2020-11-22 18:55:11 +0100

    footer and header and while loop

--- single.php
+++ single.php
@@ -1,4 +1,12 @@
-<h2>
-    <?php the_title(); ?>
-</h2>
-<p><?php the_content(); ?></p>
+
+<?php
+    get_header();
+    while(have_posts()):
+        the_post();
+?>
+    <h2><?php the_title(); ?></h2>
+    <p><?php the_content(); ?></p>
+<?php
+    endwhile;
+    get_footer();
+?>
