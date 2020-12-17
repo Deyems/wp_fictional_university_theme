e13c45ebb7dd1292dbe15b63ffe7ac65783e2033
deyems <adeyemisola11@gmail.com>
2020-12-17 13:54:16 +0100

    adding more functions to control each Page

--- page.php
+++ page.php
@@ -4,14 +4,14 @@
         the_post();
 ?>
     <div class="page-banner">
-    <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('images/ocean.jpg');?>) "></div>
-    <div class="page-banner__content container container--narrow">
-      <h1 class="page-banner__title"><?php the_title(); ?></h1>
-      <div class="page-banner__intro">
-        <p>Dont forget this SUBTITLE later</p>
-      </div>
+        <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('images/ocean.jpg');?>) "></div>
+        <div class="page-banner__content container container--narrow">
+        <h1 class="page-banner__title"><?php the_title(); ?></h1>
+        <div class="page-banner__intro">
+            <p>Dont forget this SUBTITLE later</p>
+        </div>
+        </div>
     </div>
-  </div>
 
   <div class="container container--narrow page-section">
     <?php
