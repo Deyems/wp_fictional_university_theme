b14c0c90fde4d44a41d483e21044c045a801f72e
deyems <adeyemisola11@gmail.com>
2020-12-21 19:24:48 +0100

    ordering events and filtering out past events

--- themes/fictional-university-theme/front-page.php
+++ themes/fictional-university-theme/front-page.php
@@ -16,10 +16,23 @@
         <div class="full-width-split__inner">
           <h2 class="headline headline--small-plus t-center">Upcoming Events</h2>
           <?php
+            $today = date('Ymd');
             $args = [
-                'post_type' => 'event',
-                'posts_per_page' => 10,
+              'post_type' => 'event',
+              'posts_per_page' => 10,
+              'orderby' => 'meta_value_num',
+              'meta_key' => 'event_date',
+              'order' =>  'ASC',
+              'meta_query' => [
+                [
+                  'key' => 'event_date',
+                  'value' => $today,
+                  'compare' => '>=',
+                  'type' => 'numeric',
+                ]
+              ]
             ];
+
             $loop = new WP_Query($args);
             while ( $loop->have_posts() ) {
                 $loop->the_post();
@@ -27,8 +40,12 @@
                 
                 <div class="event-summary">
                   <a class="event-summary__date t-center" href="<?php the_permalink(); ?>">
-                    <span class="event-summary__month"> <?php the_time('M');  ?></span>
-                    <span class="event-summary__day"> <?php the_time('d');  ?></span>
+                    <span class="event-summary__month"> 
+                      <?php $event_date = new DateTime(get_field('event_date')); 
+                        echo $event_date->format('M');
+                      ?>
+                    </span>
+                    <span class="event-summary__day"> <?php echo $event_date->format('d'); ?></span>
                   </a>
                   <div class="event-summary__content">
                     <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
