16ba3d43f3d753a71664995798beff9a8dd8403a
deyems <adeyemisola11@gmail.com>
2020-12-22 14:19:07 +0100

    add reset of data, added link to past events, made date show event-date NOT published post date

--- themes/fictional-university-theme/archive-event.php
+++ themes/fictional-university-theme/archive-event.php
@@ -8,19 +8,24 @@
                 All Events
             </h1>
             <div class="page-banner__intro">
-            <p> Our Events are listed below: here </p>
+            <p> Our Events are listed below: </p>
             </div>
         </div>
     </div>
 
     <div class="container container--narrow page-section">
-        <?php while(have_posts()): 
-        the_post();
+        <?php
+            while(have_posts()):
+            the_post();
         ?>
         <div class="event-summary">
             <a class="event-summary__date t-center" href="<?php the_permalink(); ?>">
-                <span class="event-summary__month"> <?php the_time('M');  ?></span>
-                <span class="event-summary__day"> <?php the_time('d');  ?></span>
+                <span class="event-summary__month"> <?php 
+                    $event_date = new DateTime(get_field('event_date'));
+                    echo $event_date->format('M');
+                    //the_time('M');  ?>
+                </span>
+                <span class="event-summary__day"> <?php echo $event_date->format('d');  ?></span>
             </a>
             <div class="event-summary__content">
                 <h5 class="event-summary__title headline headline--tiny">
@@ -31,9 +36,16 @@
                 </p>
             </div>
         </div>
-        <?php endwhile; ?>
+        <?php
+            endwhile;
+            wp_reset_postdata();
+        ?>
         <div>
             <?php echo paginate_links(); ?>
+            <hr class="section-break">
+        </div>
+        <div>
+            <a href="<?php echo site_url('/past-events') ?>">Past Events</a>
         </div>
     </div>
 <?php get_footer();
