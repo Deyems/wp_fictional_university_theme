863352d0b19a9e1c1e3237bea29a11a19f15bdfe
deyems <adeyemisola11@gmail.com>
2020-12-22 14:21:42 +0100

    condition to highlight nav link event for past events

--- themes/fictional-university-theme/header.php
+++ themes/fictional-university-theme/header.php
@@ -28,7 +28,9 @@
                 <a href="<?php echo site_url('/about-us') ?>">About Us</a>
               </li>
               <li><a href="#">Programs</a></li>
-              <li <?php if(get_post_type() == 'event') echo 'class="current-menu-item"'; ?>><a href="<?php echo get_post_type_archive_link('event') ?>">Events</a></li>
+              <li <?php if(get_post_type() == 'event' || is_page('past-events')) echo 'class="current-menu-item"'; ?>>
+                <a href="<?php echo get_post_type_archive_link('event') ?>">Events</a>
+              </li>
               <li><a href="#">Campuses</a></li>
               <li <?php if(get_post_type() == 'post') echo 'class="current-menu-item"'; ?>>
                 <a href="<?php echo site_url('/blog') ?>">Blog</a>
@@ -38,7 +40,9 @@
           <div class="site-header__util">
             <a href="#" class="btn btn--small btn--orange float-left push-right">Login</a>
             <a href="#" class="btn btn--small btn--dark-orange float-left">Sign Up</a>
-            <span class="search-trigger js-search-trigger"><i class="fa fa-search" aria-hidden="true"></i></span>
+            <span class="search-trigger js-search-trigger">
+              <i class="fa fa-search" aria-hidden="true"></i>
+            </span>
           </div>
         </div>
       </div>
