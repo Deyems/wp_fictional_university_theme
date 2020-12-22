4d5a5074a5d251f7d5723cf653efe1717fdc5450
deyems <adeyemisola11@gmail.com>
2020-12-22 14:21:02 +0100

    added function to query event-dates for archive-events template

--- themes/fictional-university-theme/functions.php
+++ themes/fictional-university-theme/functions.php
@@ -25,3 +25,22 @@ function university_features(){
 }
 
 add_action('after_setup_theme','university_features');
+
+function university_adjust_queries($query){
+    if(!is_admin() && is_post_type_archive('event') && $query->is_main_query()){
+        $today = date('Ymd');
+        $query->set('meta_key', 'event_date');
+        $query->set('orderby', 'meta_value_num');
+        $query->set('order', 'ASC');
+        $query->set('meta_query', [
+            [
+                'key' => 'event_date',
+                'compare' => '>=',
+                'value' => $today,
+                'type' => 'numeric',
+            ]
+        ]);
+    }
+}
+
+add_action('pre_get_posts', 'university_adjust_queries');
