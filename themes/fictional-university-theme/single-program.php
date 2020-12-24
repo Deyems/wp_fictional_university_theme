dadbbc21dcfc6585cd67524e3fecddbb5a125def
deyems <adeyemisola11@gmail.com>
2020-12-24 22:28:29 +0100

    made loop to show professors linked with programs

--- themes/fictional-university-theme/single-program.php
+++ themes/fictional-university-theme/single-program.php
@@ -8,7 +8,9 @@
     the_post();
 ?>
     <div class="page-banner">
-        <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('images/ocean.jpg');?>) "></div>
+        <div class="page-banner__bg-image" style="background-image: url(
+          <?php echo get_theme_file_uri('images/ocean.jpg');
+          ?>) "></div>
         <div class="page-banner__content container container--narrow">
             <h1 class="page-banner__title"><?php the_title(); ?></h1>
             <div class="page-banner__intro">
@@ -18,22 +20,24 @@
     </div>
     <div class="container container--narrow page-section">
         <div class="metabox metabox--position-up metabox--with-home-link">
-            <p>
-                <a class="metabox__blog-home-link" href="
-                    <?php echo get_post_type_archive_link('program'); ?>">
-                    <i class="fa fa-home" aria-hidden="true"></i>
-                    All Programs
-                </a>
-                <span class="metabox__main">
-                    <?php the_title(); ?> 
-                </span>
-            </p>
+          <p>
+              <a class="metabox__blog-home-link" href="
+                  <?php echo get_post_type_archive_link('program'); ?>">
+                  <i class="fa fa-home" aria-hidden="true"></i>
+                  All Programs
+              </a>
+              <span class="metabox__main">
+                  <?php the_title(); ?> 
+              </span>
+          </p>
         </div>
         <div class="generic-content">
-            <?php the_content(); ?>
+          <?php the_content(); ?>
         </div>
-        <!-- <hr class="section-break">
-        <h1 class="headline headline--medium">Upcoming <?php //echo get_the_title(); ?> Events</h1> -->
+        <?php
+        endwhile;
+    ?>
+
         <?php
             $today = date('Ymd');
             $args = [
@@ -97,10 +101,45 @@
             ?>
 
 
+            
+            <?php
+            $args = [
+              'post_type' => 'professor',
+              'posts_per_page' => '-1',
+              'orderby' => 'title',
+              'order' =>  'ASC',
+              'meta_query' => [
+                [
+                    'key' => 'related_programs',
+                    'compare' => 'LIKE',
+                    'value' => '"'. get_the_ID() .'"',
+                ],
+              ]
+            ];
+            $relatedProfessor = new WP_Query($args);
+
+            if($relatedProfessor->have_posts()){
+                echo '<hr class="section-break">
+                <h1 class="headline headline--medium">'. get_the_title() .' Lecturers</h1>';
+                echo "<ul class='professor-cards'>";
+            }
+            while ( $relatedProfessor->have_posts() ) {
+              $relatedProfessor->the_post();
+              ?>
+              
+              <li class="professor-card__listitem">
+                <a class="professor-card" href="<?php the_permalink(); ?>">
+                  <img class="professor-card__image" src="<?php the_post_thumbnail_url('professorLandscape'); ?>" alt="">
+                  <span class="professor-card__name"><?php the_title(); ?></span>
+                </a>
+              </li>
+            <?php
+              }
+              echo "</ul>";
+              wp_reset_postdata();
+            ?>
     </div>
-<?php
-    endwhile;
-?>
+
     
 <?php
     get_footer();
