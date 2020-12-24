44e2ed6648a1339bad2d0e1ae29f78012045b2e3
deyems <adeyemisola11@gmail.com>
2020-12-24 22:27:07 +0100

    adding image functions, and extract logic of page Banner for entire app

--- themes/fictional-university-theme/functions.php
+++ themes/fictional-university-theme/functions.php
@@ -22,6 +22,10 @@ function university_features(){
     register_nav_menu('footerLocationOne', 'Footer Location One');
     register_nav_menu('footerTwoLocation', 'Foot Location Two');
     add_theme_support('title-tag');
+    add_theme_support('post-thumbnails');
+    add_image_size('professorLandscape', 400, 260, true);
+    add_image_size('professorPortrait', 480, 650, true);
+    add_image_size('pageBanner', 1500, 350, true);
 }
 
 add_action('after_setup_theme','university_features');
@@ -51,3 +55,30 @@ function university_adjust_queries($query){
 }
 
 add_action('pre_get_posts', 'university_adjust_queries');
+?>
+
+<?php
+    function pageBanner(){
+    ?>
+    <div class="page-banner">
+        <div class="page-banner__bg-image" style="background-image: url(
+            <?php 
+                $bannerImg = get_field('page_banner_background_image');
+                if(!$bannerImg){
+                    echo get_theme_file_uri('images/ocean.jpg');
+                }else{
+                    echo $bannerImg['sizes']['pageBanner'];
+                }
+
+            ?>) ">
+        </div>
+        <?php //var_dump($bannerImg); ?>
+        <div class="page-banner__content container container--narrow">
+            <h1 class="page-banner__title"><?php the_title(); ?></h1>
+            <div class="page-banner__intro">
+                <p><?php the_field('page_banner_subtitle'); ?></p>
+            </div>
+        </div>
+    </div>
+<?php 
+}
