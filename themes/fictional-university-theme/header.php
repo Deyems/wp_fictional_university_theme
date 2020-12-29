217055ce9d7c403344f3a719f628d3fc667497cb
deyems <adeyemisola11@gmail.com>
2020-12-29 22:32:51 +0100

    added link to campus on the nav

--- themes/fictional-university-theme/header.php
+++ themes/fictional-university-theme/header.php
@@ -25,15 +25,17 @@
             ?>
             <ul>
               <li <?php if(is_page('about-us') || wp_get_post_parent_id(0) == 13) echo 'class="current-menu-item"'; ?>>
-                <a href="<?php echo site_url('/about-us') ?>">About Us</a>
+                <a href="<?php echo site_url('/about-us'); ?>">About Us</a>
               </li>
               <li <?php if(get_post_type() == 'program') echo 'class="current-menu-item"'; ?>>
-                <a href="<?php echo get_post_type_archive_link('program') ?>">Programs</a>
+                <a href="<?php echo get_post_type_archive_link('program'); ?>">Programs</a>
               </li>
               <li <?php if(get_post_type() == 'event' || is_page('past-events')) echo 'class="current-menu-item"'; ?>>
-                <a href="<?php echo get_post_type_archive_link('event') ?>">Events</a>
+                <a href="<?php echo get_post_type_archive_link('event'); ?>">Events</a>
               </li>
-              <li><a href="#">Campuses</a></li>
+              <li <?php if(get_post_type() == 'campus') echo 'class="current-menu-item"'; ?>>
+              <a href="<?php echo get_post_type_archive_link('campus'); ?>">Campuses</a>
+              </li>
               <li <?php if(get_post_type() == 'post') echo 'class="current-menu-item"'; ?>>
                 <a href="<?php echo site_url('/blog') ?>">Blog</a>
               </li>
