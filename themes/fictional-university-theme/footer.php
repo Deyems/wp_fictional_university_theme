4e2c6d7fdd0c117d8cc28b595e89371d9047976f
deyems <adeyemisola11@gmail.com>
2021-01-02 21:22:31 +0100

    adding overlay for search results

--- themes/fictional-university-theme/footer.php
+++ themes/fictional-university-theme/footer.php
@@ -68,6 +68,23 @@
     </div>
   </div>
 </footer>
+<div class="search-overlay">
+  <div class="search-overlay__top">
+    <div class="container">
+      <i class="fa fa-search search-overlay__icon" aria-hidden="true"></i>
+      <input type="text" class="search-term" placeholder="Enter what you are looking for?" id="search-item">
+      <i class="fa fa-window-close search-overlay__close" aria-hidden="true"></i>
+    </div>
+  </div>
+  
+  <div class="container">
+    <div class="search-overlay__results">
+      Hello Here
+    </div>
+  </div>
+
+</div>
+
 <?php wp_footer(); ?>
 </body>
 </html>
