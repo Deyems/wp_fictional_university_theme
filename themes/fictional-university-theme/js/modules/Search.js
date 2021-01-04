19ba3e104f9a2b2e6db9a540df8cc5a58cb11636
deyems <adeyemisola11@gmail.com>
2021-01-04 13:06:55 +0100

    handling content search with rest api on wp

--- themes/fictional-university-theme/js/modules/Search.js
+++ themes/fictional-university-theme/js/modules/Search.js
@@ -14,6 +14,8 @@ class Search{
         this.escKeyNumber = 27;
         this.events();
         this.isOverLayOpen = false;
+        this.isSpinnerVisible = false;
+        this.previousVal;
         this.typingTimer;
     }
 
@@ -23,19 +25,45 @@ class Search{
         this.openButton.on("click", this.openOverlay.bind(this));
         this.closeButton.on("click", this.closeOverlay.bind(this));
         $(document).on("keydown", this.keyPressDispatcher.bind(this));
-        this.searchField.on("keydown", this.searchDB.bind(this));
+        this.searchField.on("keyup", this.searchDB.bind(this));
     }
 
     //Methods
     searchDB(){
         // console.log('Javascript typing is cool!');
-        clearTimeout(this.typingTimer);
-        this.typingTimer = setTimeout(this.getResults.bind(this),2000);
+        if(this.previousVal !== this.searchField.val()){
+            clearTimeout(this.typingTimer);
+
+            if(this.searchField.val()){
+                //Add a loader after typing before showing results
+                if(!this.isSpinnerVisible){
+                    this.resultsDiv.html("<div class='spinner-loader'></div>");
+                    this.isSpinnerVisible = true;
+                }
+                this.typingTimer = setTimeout(this.getResults.bind(this),2000);
+            }else{
+                this.resultsDiv.html('');
+                // this.isSpinnerVisible = false;
+            }
+
+            
+        }
+        this.previousVal = this.searchField.val();
     }
 
     getResults(){
         // console.log('My belle o');
-        this.resultsDiv.html("Imageing we are here now!");
+        // console.log(this.searchField.val());
+        let url = "http://localhost:10008/wp-json/wp/v2/posts?search="+ this.searchField.val();
+        $.getJSON(url, results => {
+            this.resultsDiv.html(`
+            <h2 class="search-overlay__section-title">General Information</h2>
+            <ul class="link-list min-list">
+            ${results.map(item => `<li><a href="${item.link}">${item.title.rendered}</a></li>`).join('')}
+            </ul>`);
+        });
+        // <li><a href="${results[0].link}">${results[0].title.rendered}</a></li>
+        this.isSpinnerVisible = false;
     }
 
     openOverlay(){
@@ -54,7 +82,7 @@ class Search{
 
     keyPressDispatcher(e){
         // console.log('Body is shaking'+ e.keyCode);
-        if(e.keyCode == this.skeyNumber && !this.isOverLayOpen){
+        if(e.keyCode == this.skeyNumber && !this.isOverLayOpen && !$("input, textarea").is(':focus')){
             this.openOverlay();
         }
 
