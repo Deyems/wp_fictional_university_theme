81ebdb84592f0d16a69b7be4a0bdcd87eff63e77
deyems <adeyemisola11@gmail.com>
2021-01-05 00:33:24 +0100

    call to localize scripts

--- themes/fictional-university-theme/functions.php
+++ themes/fictional-university-theme/functions.php
@@ -14,6 +14,9 @@ function university_files(){
         wp_enqueue_script('main-university-js', get_theme_file_uri('/bundled-assets/scripts.bc49dbb23afb98cfc0f7.js'), NULL,'1.0', true);
         wp_enqueue_style('our-main-styles', get_theme_file_uri('/bundled-assets/styles.bc49dbb23afb98cfc0f7.css'));
     }
+    wp_localize_script('main-university-js', 'universityData', [
+        'root_url' => get_site_url(),
+    ]);
 }
 
 add_action('wp_enqueue_scripts', 'university_files');
