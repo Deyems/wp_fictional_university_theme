067c9eb1f4b726442dd7ff8e2bb7c0b2b206b807
deyems <adeyemisola11@gmail.com>
2021-01-09 12:16:43 +0100

    form to aid search

---
+++ themes/fictional-university-theme/searchform.php
@@ -0,0 +1,7 @@
+<form class="search-form" action="<?php echo esc_url(site_url('/')); ?>" method="get">
+    <label class="headline headline--medium" for="s">Do you need anything?</label>    
+    <div class="search-form-row">
+        <input placeholder="What do you want to know?" class="s" type="search" name="s" id="s">
+        <button class="search-submit" type="submit">Search</button>
+    </div>
+</form>
