663e2726fb6e1f4c363d3dcfc768a114c0a73dcb
deyems <adeyemisola11@gmail.com>
2021-01-09 12:18:31 +0100

    adjusted front-page to show events

--- themes/fictional-university-theme/front-page.php
+++ themes/fictional-university-theme/front-page.php
@@ -36,7 +36,7 @@
             $loop = new WP_Query($args);
             while ( $loop->have_posts() ) {
               $loop->the_post();
-              get_template_part('template-parts/event');
+              get_template_part('template-parts/content-event');
               }
               wp_reset_postdata();
           ?>
