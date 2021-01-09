a6ee6f046379cead869e7d4afe897bf38258e447
deyems <adeyemisola11@gmail.com>
2021-01-09 12:17:30 +0100

    page search with form

---
+++ themes/fictional-university-theme/page-search.php
@@ -0,0 +1,68 @@
+<?php
+    get_header();
+    while(have_posts()):
+        the_post();
+?>
+    <?php
+        pageBanner([
+            'title' => 'This is my Logo',
+        ]);
+    ?>
+
+    <div class="container container--narrow page-section">
+    <?php
+        $parentId = wp_get_post_parent_id(get_the_ID());
+        if($parentId):
+    ?>
+        <div class="metabox metabox--position-up metabox--with-home-link">
+        <p>
+            <a class="metabox__blog-home-link" href="<?php echo get_permalink($parentId) ?>">
+                <i class="fa fa-home" aria-hidden="true"></i> Back to 
+                <?php echo get_the_title($parentId) ?>
+            </a>
+            <span class="metabox__main"><?php the_title(); ?></span>
+        </p>
+        </div>
+    
+    <?php 
+        endif;
+        $testArray = get_pages(
+            [
+            'child_of' => get_the_ID()
+            ]
+        );
+
+    if($parentId || $testArray): ?>
+    <div class="page-links">
+      <h2 class="page-links__title">
+        <a href="<?php echo get_permalink($parentId) ?>">
+            <?php echo get_the_title($parentId); ?>
+        </a>
+      </h2>
+      <ul class="min-list">
+        <?php
+            if($parentId){
+                $childOf = $parentId;
+            }else{
+                $childOf = get_the_ID();
+            }
+            wp_list_pages([
+                'title_li' => NULL,
+                'child_of' => $childOf,
+                'sort_column' => 'menu_order',
+            ]);
+        ?>
+      </ul>
+    </div>
+    <?php endif; ?>
+
+    <div class="generic-content">
+      <?php get_search_form(); ?>
+    </div>
+
+  </div>
+
+<?php
+    endwhile;
+    get_footer();
+?>
