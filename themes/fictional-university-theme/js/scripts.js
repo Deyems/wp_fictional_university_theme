e571e3035e5d0d753780a5b2a1948bfc26e82f59
deyems <adeyemisola11@gmail.com>
2021-01-10 20:05:25 +0100

    loading notes module

--- themes/fictional-university-theme/js/scripts.js
+++ themes/fictional-university-theme/js/scripts.js
@@ -5,12 +5,14 @@ import MobileMenu from "./modules/MobileMenu"
 import HeroSlider from "./modules/HeroSlider"
 import GoogleMap from "./modules/GoogleMap";
 import Search from "./modules/Search";
+import MyNotes from "./modules/MyNotes";
 
 // Instantiate a new object using our modules/classes
 const mobileMenu = new MobileMenu();
 const heroSlider = new HeroSlider();
 const googleMap = new GoogleMap();
 const magicSearch = new Search();
+const myNotes = new MyNotes();
 
 // Allow new JS and CSS to load in browser without a traditional page refresh
 if (module.hot) {
