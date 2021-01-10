4df2cd6e852b91b11ac233844e1e2f4ce4c5f732
deyems <adeyemisola11@gmail.com>
2021-01-10 23:42:22 +0100

    feature to prevent malicious code

--- themes/fictional-university-theme/page-my-notes.php
+++ themes/fictional-university-theme/page-my-notes.php
@@ -19,6 +19,7 @@
             <input class="new-note-title" type="text" placeholder="Title">
             <textarea class="new-note-body" placeholder="Your note here..." name="" id="" cols="30" rows="10"></textarea>
             <span class="submit-note">Create Note</span>
+            <span class="note-limit-message">Note limit reached: delete an existing note to add new notes</span>
         </div>
         <ul class="min-list link-list" id="my-notes">
             <?php
@@ -31,11 +32,11 @@
                     $userNotes->the_post();
             ?>
             <li data-id="<?php the_ID() ?>">
-                <input readonly class="note-title-field" value="<?php echo esc_attr(get_the_title()) ?>" type="text">
+                <input readonly class="note-title-field" value="<?php echo str_replace('Private: ','', esc_attr(get_the_title()))  ?>" type="text">
                 <span class="edit-note"><i class="fa fa-pencil" aria-hidden="true"></i> Edit</span>
                 <span class="delete-note"><i class="fa fa-trash-o" aria-hidden="true"></i> Delete</span>
                 <textarea readonly class="note-body-field" name="" id="" cols="30" rows="10">
-                    <?php echo esc_attr(wp_strip_all_tags(get_the_content())); ?>
+                    <?php echo esc_textarea(wp_strip_all_tags(get_the_content())); ?>
                 </textarea>
                 <span class="update-note btn btn--blue btn--small"><i class="fa fa-arrow-right" aria-hidden="true"></i> Update</span>
             </li>
