8d39f2315f4d81a00bfc4da7ed1a329cddf44ab8
deyems <adeyemisola11@gmail.com>
2021-01-11 14:56:07 +0100

    adding template to show heart and option to send to wp_database

--- themes/fictional-university-theme/single-professor.php
+++ themes/fictional-university-theme/single-professor.php
@@ -15,6 +15,38 @@
                 <?php the_post_thumbnail('professorPortrait'); ?>
                 </div>
                 <div class="two-thirds">
+                    <?php 
+                        $likeCount = new WP_Query([
+                            'post_type' => 'like',
+                            'meta_query' => [
+                                [
+                                    'key' => 'liked_professor_id',
+                                    'compare' => '=',
+                                    'value' => get_the_ID(),
+                                ]
+                            ]
+                        ]);
+                        $existStatus = 'no';
+                        $existQuery = new WP_Query([
+                            'author' => get_current_user_id(),
+                            'post_type' => 'like',
+                            'meta_query' => [
+                                [
+                                    'key' => 'liked_professor_id',
+                                    'compare' => '=',
+                                    'value' => get_the_ID(),
+                                ]
+                            ]
+                        ]);
+                        if($existQuery->found_posts){
+                            $existStatus = 'yes';
+                        }
+                    ?>
+                        <span class="like-box" data-exists="<?php echo $existStatus; ?>">
+                            <i class="fa fa-heart-o" aria-hidden="true"></i>
+                            <i class="fa fa-heart" aria-hidden="true"></i>
+                            <span class="like-count"><?php echo $likeCount->found_posts; ?></span>
+                        </span>
                 <?php the_content(); ?>
                 </div>
             </div>
