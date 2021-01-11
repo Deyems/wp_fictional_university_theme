a30991ad81f5e6107dd310a40b0e050796667449
deyems <adeyemisola11@gmail.com>
2021-01-11 14:56:53 +0100

    setting up the like Module to create and delete likes

---
+++ themes/fictional-university-theme/js/modules/Like.js
@@ -0,0 +1,49 @@
+import $, { ajax } from 'jquery';
+
+class Like{
+    constructor(){
+        this.events();
+    }
+
+    events(){
+        $(".like-box").on("click", this.ourClickDispatcher.bind(this));
+    }
+
+    //Methods
+    ourClickDispatcher(e){
+        let currentLikeBox = $(e.target).closest(".like-box");
+        if(currentLikeBox.data('exists') == 'yes'){
+            this.deleteLike();
+        }else{
+            this.createLike();
+        }
+    }
+
+    createLike(){
+        $(ajax({
+            url: universityData.root_url + '/wp-json/university/v1/likecount',
+            type: 'POST',
+            success: (response) => {
+                console.log(response);
+            },
+            error: (response) => {
+                console.log(response);
+            }
+        }))
+    }
+
+    deleteLike(){
+        $(ajax({
+            url: universityData.root_url + '/wp-json/university/v1/likecount',
+            type: 'DELETE',
+            success: (response) => {
+                console.log(response);
+            },
+            error: (response) => {
+                console.log(response);
+            }
+        }))
+    }
+}
+
+export default Like;
